9eeef545c1dd8d75d080f4e5c35f7096eb6de859
Mohan-blog <mohan.seelamneni@gmail.com>
2020-01-29 20:22:45 +0530

    third commit

---
+++ app/src/main/java/com/adteccorp/registeract/ThirdAct.java
@@ -0,0 +1,14 @@
+package com.adteccorp.registeract;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_third );
+    }
+}
